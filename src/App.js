b0b39e465ad73aebc20f133beee53adb2eafa910
qmdl980 <qmdl98056@gmail.com>
2021-04-02 11:06:28 +0900

    React create Component folder

--- src/App.js
+++ src/App.js
@@ -1,43 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import { render } from '@testing-library/react';
 import { Component } from 'react';
-
-class Subject extends Component {
-  render(){
-    return (
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    );
-  }
-}
-
-class TOC extends Component{
-  render(){
-    return(
-      <nav>
-        <ul>
-        <li><a href="1.html">HTML</a></li>
-        <li><a href="2.html">CSS</a></li>
-        <li><a href="1.html">JavaScript</a></li>
-      </ul>
-      </nav>
-    );
-  }
-}
-
-class Content extends Component{
-  render(){
-    return(
-      <article>
-          <h2>{this.props.title}</h2>
-              {this.props.desc}
-          </article>
-    );
-  }
-}
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
 
 class App extends Component {
   render(){
